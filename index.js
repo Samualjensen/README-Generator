b44782da9c4b6ea99cf2a8b8c2f54e920b60aef4
Sam Jensen <samual.jensen@gmail.com>
2022-12-20 10:24:32 -0800

    starting to add questions/restructer folders

--- index.js
+++ index.js
@@ -1,7 +1,118 @@
-// TODO: Include packages needed for this application
+// node modules
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// link to where README is generated
+const generateReadMe = require('./utils/generateMarkdown.js');
+
+// questions array
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Title';
+            message: 'What is your project title?';
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a project title!')
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: '';
+            message: '';
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter !')
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: '';
+            message: '';
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter !')
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: '';
+            message: '';
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter !')
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: '';
+            message: '';
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter !')
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: '';
+            message: '';
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter !')
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: '';
+            message: '';
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter !')
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: '';
+            message: '';
+            validate: answerInput => {
+                if (answerInput) {
+                    return true;
+                } else {
+                    console.log('Please enter !')
+                }
+            }
+        },
+]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
